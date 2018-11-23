32a01e7825a68afa25c31c967e19aa2bc24ee280
Kin <kintuda17@gmail.com>
2018-11-22 22:26:25 -0200

    Add project files.

---
+++ enzo-estoqueV1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="enzo_estoqueV1.MvcApplication" Language="C#" %>
---
+++ enzo-estoqueV1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(enzo_estoqueV1.Startup))]
+namespace enzo_estoqueV1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
